7fa90e43086df6b2f35c7f67b08304e723174013
Filip Arsić <filip.arsic@incode.com>
2024-03-06 20:52:19 +0100

    style

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import * as github from '@actions/github';
-import * as core from '@actions/core';
+import * as github from '@actions/github'
+import * as core from '@actions/core'
 
 /**
  * The main function for the action.
@@ -7,18 +7,18 @@ import * as core from '@actions/core';
  */
 export async function run(): Promise<void> {
   try {
-    const githubToken = core.getInput('github_token', { required: true });
-    
+    const githubToken = core.getInput('github_token', { required: true })
+
     const assignee = core.getInput('assignee', { required: true })
-    
+
     const number = github.context.issue.number
 
-    const client = new github.GitHub(githubToken);
+    const client = new github.GitHub(githubToken)
 
     await client.issues.addAssignees({
       assignee,
       issue_number: number
-    });
+    })
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
